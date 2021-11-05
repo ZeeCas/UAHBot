93607009dfd52dd42a091020021871b49509cb4a
cas <zeederpmaster@gmail.com>
2021-11-05 17:23:39 -0500

    second commit

--- main.py
+++ main.py
@@ -4,13 +4,52 @@ import re
 import sys, getopt
 import discord
 import random
+import requests
 from discord.ext import commands
 import asyncio
 import urllib.request
+import youtube_dl
 from dotenv import load_dotenv
 load_dotenv()
 
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
 
 class Bot:
 
@@ -64,20 +103,11 @@ class Bot:
                     log = open("say-log.txt","a")
                     log.write(f"{self.bot.user.name} said:{whole_message}\n")
                     log.close()  
-                elif command == "scream":
-                    await channel.send("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHH")
-                    await message.delete()
-                elif command == "react":
-                    for emoji in args:
-                        if emoji == "react":
-                            print("e")
-                        else:
-                            await message.add_reaction(emoji)
                 elif command == "8ball":
                     responses = ["No","Mayhaps","Ask again later","Are you dumb? Absolutely not.","Of course silly!","Its a little foggy","It's certain","Huh? I didn't catch that","Never. Not in a million years","I don't see why not?","That just isn't gonna work."]
                     await channel.send(random.choice(responses))
                 elif command == "quote":
-                    quotes = ["\"Do not throw babies\" - Dr.Hannah","\"To know your enemy, you must become your enemy.\" - Some Chinese guy","\“Don’t marry a farrier,\” - Dr. Berry","\“If Kamala Harris becomes president, that will make her the hottest president in history— because I’m not gay\”","\"I want to be a bee ... I want to be a praying mantis, that's how I wanna go\" -- Nolan","\“I want somebody to punish me\” ~Luke","\"I think we should kill them (ewoks) long and painfully\" Dr.Baudry","\"it just needs more quotes to properly work i think\" - Cas","\"Muck!\" \"Muck?\"","\"Coyote of the sandwhich\" -Hayley"]
+                    quotes = ["\"Do not throw babies\" - Dr.Hannah","\"To know your enemy, you must become your enemy.\" - Some Chinese guy","\“Don’t marry a farrier,\” - Dr. Berry","\“If Kamala Harris becomes president, that will make her the hottest president in history— because I’m not gay\”","\"I want to be a bee ... I want to be a praying mantis, that's how I wanna go\" -- Nolan","\“I want somebody to punish me\” ~Luke","\"I think we should kill them (ewoks) long and painfully\" Dr.Baudry","\"it just needs more quotes to properly work i think\" - Cas","\"Muck!\" \"Muck?\"","\"Coyote of the sandwhich\" -Hayley","\“Don’t you dare steal my turkey thug hat!\”"]
                     await channel.send(random.choice(quotes))
                 elif command =="yt":
                     query = ""
@@ -86,9 +116,49 @@ class Bot:
                     query = query.replace(" ","+")
                     html = urllib.request.urlopen(f"https://www.youtube.com/results?search_query={query}")
                     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-                    await channel.send(f"https://www.youtube.com/watch?v={video_ids[1]}")
+                    await channel.send(f"https://www.youtube.com/watch?v={video_ids[0]}")
                 elif command == "help":
-                    await channel.send("The commands are : say,8ball,quote,react, and yt. yt allows you to search for youtube videos, it chooses the first result.")
+                    await channel.send("```The commands are : say, \n8ball, \nquote, \ndog,\ncat, \ncoc, \nyt. yt allows you to search for youtube videos, it chooses the first result.```")
+                elif command == "dance":
+                    await channel.send("ᕕ(⌐■_■)ᕗ ♪♬")
+                elif command =="dog":
+                    api = "https://dog.ceo/api/breeds/image/random"
+                    data = requests.get(api)
+                    data = data.json()
+                    await channel.send(data['message'])
+                    
+                elif command =="cat":
+                    api = "https://api.thecatapi.com/v1/images/search"
+                    data = requests.get(api)
+                    data = data.json()
+                    await channel.send(data[0]['url'])
+                elif command == "coc":
+                    listoguide = ["https://clashofclans.fandom.com/wiki/Flammy%27s_Strategy_Guides","https://medium.com/mr-ways-guide-to-clash-of-clans/clash-of-clans-the-ultimate-beginners-guide-830f6d7e0a74","https://houseofclashers.com/wiki/en/clash-of-clans-builder-base/strategy-guide/beginners-guide/"]
+                    await channel.send(random.choice(listoguide))
+                elif command == "poll":
+                    whole_message = ""
+                    for i in args[1:]:
+                        whole_message = whole_message + " " + i
+                    msg = await channel.send(whole_message)
+                    await msg.add_reaction("✅")
+                    await msg.add_reaction("🚫")
+                    await message.delete()
+                elif command == "join":
+                    VC = message.author.voice.channel
+                    global client
+                    client = await VC.connect()
+                elif command == "leave":
+                    await client.disconnect()
+                elif command == "play":
+                    list = ["heman.mp3","donda.mp3","everlong.mp3","bleachers.mp3"]
+                    client.play(discord.FFmpegPCMAudio("heman.mp3"), after=lambda e: print('done', e))
+                elif command == "plyt":
+                    filename = await YTDLSource.from_url(args[1], loop=bot.loop)
+                    client.play(discord.FFmpegPCMAudio(filename))
+                elif command == "pause":
+                    client.pause()
+                elif command == "resume":
+                    client.resume()
                 elif command == "quit":
                     print("Quitting")
                     os._exit(0) 
